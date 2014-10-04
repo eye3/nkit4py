dd7b3b251d41df3b56aeab58b1b05b7f86004b3a
Boris T. Darchiev <boris.darchiev@gmail.com>
2014-10-04 21:25:02 +0400

    Release 2.0.0: Readme fix

--- test/testnkit.py
+++ test/testnkit.py
@@ -319,24 +319,24 @@ if persons_etalon != persons:
     sys.exit(1)
 
 
-mapping = ["/person",
-    {
-        "/photos": ["/*", {
-            "/ -> url" : "string",
-            "/width": "integer|0",
-            "/height": "integer|0"
-        }],
-        "/name": "string"
-    }
-]
-
-mappings = {"persons": mapping}
-
-builder = Xml2VarBuilder(mappings)
-builder.feed(xml_string)
-result = builder.end()
-persons = result["persons"]
-pring_json(persons)
+# mapping = ["/person",
+#     {
+#         "/photos": ["/*", {
+#             "/ -> url" : "string",
+#             "/width": "integer|0",
+#             "/height": "integer|0"
+#         }],
+#         "/name": "string"
+#     }
+# ]
+#
+# mappings = {"persons": mapping}
+#
+# builder = Xml2VarBuilder(mappings)
+# builder.feed(xml_string)
+# result = builder.end()
+# persons = result["persons"]
+# pring_json(persons)
 
 print "ok"
 sys.exit(0)
